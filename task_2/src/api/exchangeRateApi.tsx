f0d2358f0018415d442d9a05d5dc44107fd91fef
ghostmodd <ghost1mod@gmail.com>
2023-10-07 17:01:37 +0300

    feat: solve second problem

---
+++ task_2/src/api/exchangeRateApi.tsx
@@ -0,0 +1,34 @@
+class ExchangeRateApi {
+  private _baseUrl: string
+  private _apiKey: string
+
+  constructor(baseUrl: string, apiKey: string) {
+    this._baseUrl = baseUrl
+    this._apiKey = apiKey
+  }
+
+  _getError(res: Response) {
+    if (!res.ok) {
+      return res
+    }
+  }
+
+  _getJSON(res: Response) {
+    if (res.ok) {
+      return res.json()
+    }
+
+    return this._getError(res)
+  }
+
+  getCurrencyRate(currency: string) {
+    return fetch(`${this._baseUrl}/${this._apiKey}/latest/${currency}`, {
+      method: "GET",
+    })
+      .then((res) => {
+        return this._getJSON(res)
+      })
+  }
+}
+
+export const exchangeRateApi = new ExchangeRateApi("https://v6.exchangerate-api.com/v6","f48a1831213c6e1a60eb93df");
---
+++ task_2/src/lib/Converter.svelte
@@ -0,0 +1,112 @@
+<script lang="ts">
+  import {exchangeRateApi} from "../api/exchangeRateApi";
+
+  const inputData = {
+    firstField: "",
+    secondField: "",
+  }
+
+  const currencies = {
+    firstCurrency: {
+      name: "",
+      rates: {},
+    },
+    secondCurrency: {
+      name: "",
+      rates: {},
+    },
+  }
+
+  let currenciesList = ["USD"]
+
+  exchangeRateApi.getCurrencyRate("USD")
+    .then((currenciesData) => {
+      currencies.firstCurrency.name = currenciesData["base_code"]
+      currencies.firstCurrency.rates = currenciesData["conversion_rates"]
+
+      currencies.secondCurrency.name = currenciesData["base_code"]
+      currencies.secondCurrency.rates = currenciesData["conversion_rates"]
+
+      currenciesList = Object.keys(currenciesData["conversion_rates"])
+    })
+    .catch((err) => {
+      throw Error(`Ошибка! ${err.code}`)
+    })
+
+  function handleInputCurrency(evt: InputEvent) {
+    inputData[evt.target.id] = evt.target.value
+
+    if(evt.target.id === "firstField") {
+      inputData["secondField"] = (Number(inputData["firstField"]) * Number(currencies["firstCurrency"].rates[currencies["secondCurrency"].name])).toString()
+    }
+
+    if(evt.target.id === "secondField") {
+      inputData["firstField"] = (Number(inputData["secondField"]) / Number(currencies["firstCurrency"].rates[currencies["secondCurrency"].name])).toString()
+    }
+  }
+
+  function handleSelectCurrency(evt: InputEvent) {
+    currencies[evt.target.id].name = evt.target.value
+
+    if(evt.target.id === "firstCurrency") {
+      exchangeRateApi.getCurrencyRate(evt.target.value)
+        .then((currencyData) => {
+          currencies[evt.target.id].rates = currencyData["conversion_rates"]
+        })
+        .then(() => {
+          console.log(currencies)
+        })
+    }
+  }
+</script>
+
+<section class="currency-calculator">
+  <div class="currency">
+    <select class="currency-select input" id="firstCurrency" on:change={handleSelectCurrency}>
+      {#each currenciesList as item}
+        <option value={item}>{item}</option>
+      {/each}
+    </select>
+
+    <input class="currency__input input" id="firstField" value={inputData.firstField}  type="number" placeholder="Введите количество" on:input={handleInputCurrency}>
+  </div>
+
+  <div class="currency">
+    <select class="currency-select input" id="secondCurrency" on:change={handleSelectCurrency}>
+      {#each currenciesList as item}
+        <option value={item}>{item}</option>
+      {/each}
+    </select>
+
+    <input class="currency__input input" id="secondField" value={inputData.secondField} type="number" placeholder="Введите количество" on:input={handleInputCurrency}>
+  </div>
+</section>
+
+<style>
+  .currency-calculator {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .currency {
+    display: flex;
+    gap: 10px;
+  }
+
+  .currency__input {
+    width: 70%;
+  }
+
+  .input {
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    background-color: #1a1a1a;
+    cursor: pointer;
+    transition: border-color 0.25s;
+  }
+</style>
